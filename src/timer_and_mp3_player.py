7d77802d0ce9146c43e4eaad48ef03e1f553fb7d
maciej-witkowski <maciej.jan.witkowski@gmail.com>
2020-12-02 22:46:09 +0100

    class TimerAndMP3Player - not implemented methods

---
+++ src/timer_and_mp3_player.py
@@ -0,0 +1,13 @@
+class TimerAndMP3Player:
+
+    def getTime(self):
+        pass
+
+    def playWavFile(self, file):
+        pass
+
+    def wavWasPlayed(self, file):
+        pass
+
+    def resetWav(self, file):
+        pass
